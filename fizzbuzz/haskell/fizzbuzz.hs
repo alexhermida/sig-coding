f09789c72941d9eb8166b9d4e06ab7340e127ab7
Alex Hermida <alex.hermida@healx.io>
2020-11-26 22:19:03 +0100

    Fizzbuzz full working

--- fizzbuzz/haskell/fizzbuzz.hs
+++ fizzbuzz/haskell/fizzbuzz.hs
@@ -1,20 +1,4 @@
--- hell_world = "Hello World"
--- numbers = [1..10]
-
--- -- printNumbers x = print (show x)
--- -- fc = map printNumbers numbers
--- printLst :: [Int] -> String
--- printLst [] = ""
--- printLst [x] = (show x)
--- printLst (x:xs) = (show x) ++ " " ++ printLst xs
-
--- main :: IO()
--- main = mapM_ putStrLn $ map print [1..8]
--- --     map printNumbers numbers
--- -- main = print (hell_world)
--- -- main = putStrLn hell_world
-
--- printItem [] = return ()
+printItem [] = return ()
 printItem (x:xs) = putStrLn x >> printItem xs
 
 fizzBuzz :: Int -> String
