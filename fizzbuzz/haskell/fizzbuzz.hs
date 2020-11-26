b8d9849233bdd70d23dbc59628d8a0b40faa05bd
Alex Hermida <alex.hermida@healx.io>
2020-11-26 22:02:37 +0100

    Fizzbuzz first version

--- fizzbuzz/haskell/fizzbuzz.hs
+++ fizzbuzz/haskell/fizzbuzz.hs
@@ -1,6 +1,27 @@
-main =  printNumbers
+-- hell_world = "Hello World"
+-- numbers = [1..10]
 
-printNumbers  =  putStrLn "hello"
+-- -- printNumbers x = print (show x)
+-- -- fc = map printNumbers numbers
+-- printLst :: [Int] -> String
+-- printLst [] = ""
+-- printLst [x] = (show x)
+-- printLst (x:xs) = (show x) ++ " " ++ printLst xs
 
-list_numbers = [1..100]
+-- main :: IO()
+-- main = mapM_ putStrLn $ map print [1..8]
+-- --     map printNumbers numbers
+-- -- main = print (hell_world)
+-- -- main = putStrLn hell_world
 
+-- printItem [] = return ()
+printItem (x:xs) = putStrLn x >> printItem xs
+
+fizzBuzz :: Int -> String
+fizzBuzz n | n `mod` 15 == 0  = "FizzBuzz"
+       | n `mod` 3  == 0  = "Fizz"
+       | n `mod` 5  == 0  = "Buzz"
+       | otherwise        = show n
+
+main :: IO()
+main = printItem $ map fizzBuzz [1..100]
